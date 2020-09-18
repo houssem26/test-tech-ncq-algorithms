313ff966e660d1c680814f6d6dba2087a04a2871
Houssem <paytea@excoafrique.com>
2020-09-18 16:45:12 +0100

    fixed sort

--- algo3.js
+++ algo3.js
@@ -5,7 +5,7 @@ arr[0] = arr[0].substring(1);
 arr[arr.length-1] = arr[arr.length-1].slice(0, -1) 
 arr = arr.filter(e=> e !==',').map(e => parseInt(e));
 
-let sorted = arr.map((e, index) => ({index, value: e})).sort((a,b) => Math.abs(a) < Math.abs(b) ? 1 : -1)
+let sorted = arr.map((e, index) => ({index, value: e})).sort((a,b) => Math.abs(a.value) < Math.abs(b.value) ? 1 : -1)
 let S = Array(parseInt(sorted.length)).fill(0);
 let result = sorted.reduce((acc, curr) => {
   let positifSum = acc + curr.value;

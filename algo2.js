44787cf041ab5606db5adf83d7eb2ba6d6e529b4
Houssem Ben Hassen <paytea@excoafrique.com>
2020-09-21 10:43:15 +0100

    made into functions

--- algo2.js
+++ algo2.js
@@ -1,16 +1,23 @@
-const N = process.argv.slice(2)[0];
-let a = Array(N).fill(0);
-for(let i=0; i<N; i++) {
-    if(i===0) {
-        a[i]=1;
-    } else if(i===1) {
-        a[i]=2;
-    } else {
-        a[i] = a[i-1] + a[i-2]
+function generateRungsArray(n) {
+    // generates array of possible solutions for all numbers inferior to n
+    let arr = Array(n).fill(0);;
+    for(let i=0; i<n; i++) {
+        if(i===0) {
+            arr[i]=1;
+        } else if(i===1) {
+            arr[i]=2;
+        } else {
+            arr[i] = arr[i-1] + arr[i-2]
+        }
     }
+    return arr;
+
 }
-const rungs = (n) => {
-    
-    return n <3 ? n : rungs(n-1)+rungs(n-2) 
+function algo2(A,B) {
+    let max = Math.max(...A);
+    let possibleSolutions = generateRungsArray(max);
+    const b = A.map((e,index) => {
+        return possibleSolutions[e-1] % Math.pow(2, B[index])
+    })
+    return b;
 }
-console.log('possible solutions', a[N-1])
--- algo3.js
+++ algo3.js
@@ -1,21 +1,15 @@
-let rawArrayInput = process.argv.slice(2)[0];
-let arr = rawArrayInput.split(',')
-console.log(arr)
-arr[0] = arr[0].substring(1);
-arr[arr.length-1] = arr[arr.length-1].slice(0, -1) 
-arr = arr.filter(e=> e !==',').map(e => parseInt(e));
-
-let sorted = arr.map((e, index) => ({index, value: e})).sort((a,b) => Math.abs(a.value) < Math.abs(b.value) ? 1 : -1)
-let S = Array(parseInt(sorted.length)).fill(0);
-let result = sorted.reduce((acc, curr) => {
-  let positifSum = acc + curr.value;
-  let negatifSum = acc - curr.value;
-  if(Math.abs(positifSum) > Math.abs(negatifSum) ) {
-    S[curr.index] = -1;
-    return negatifSum;
-  }
-    S[curr.index] = 1;
-  return positifSum;
-}, 0)
-console.log('Set', S)
-console.log('result', result)
+function algo3(arr) {
+  // sort the input array by abs value 
+  let sorted = arr
+    .sort((a,b) => Math.abs(a.value) < Math.abs(b) ? 1 : -1)
+  // compare if the sum for each element is superior when it's multiplied by 1 or -1
+  let s = sorted.reduce((acc, curr) => {
+    let positifSum = acc + curr;
+    let negatifSum = acc - curr;
+    if(Math.abs(positifSum) > Math.abs(negatifSum) ) {
+      return negatifSum;
+    }
+    return positifSum;
+  }, 0)
+  return s;
+}
